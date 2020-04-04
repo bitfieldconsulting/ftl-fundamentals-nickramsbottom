a0e401fe779f9c6d8290edc4fd1d20d893a8a01c
Nick Ramsbottom <hi@nickramsbottom.com>
2020-04-04 16:44:39 +0100

    Implement calcualtor functionality

---
+++ 10/calculator.go
@@ -0,0 +1,21 @@
+package calculator
+
+// Add the argument integers
+func Add(a, b int) int {
+	return a + b
+}
+
+// Subtract two numbers
+func Subtract(a, b int) int {
+	return a - b
+}
+
+// Multiply two numbers
+func Multiply(a, b int) int {
+	return a * b
+}
+
+// Divide two numbers
+func Divide(a, b int) int {
+	return a / b
+}
