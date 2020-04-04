0e7d706d2d74dc4f0f0642e4ae67c296e69de08f
Nick Ramsbottom <hi@nickramsbottom.com>
2020-04-04 16:32:09 +0100

    Add more test cases

--- 08/calculator_test.go
+++ 08/calculator_test.go
@@ -13,6 +13,13 @@ func TestAdd(t *testing.T) {
 		{a: 1, b: 1, want: 2},
 		{a: 2, b: 2, want: 4},
 		{a: 6, b: 7, want: 13},
+		{a: 0, b: 7, want: 7},
+		{a: 6, b: 0, want: 6},
+		{a: -1, b: -1, want: -2},
+		{a: 3, b: -1, want: 2},
+		{a: -3, b: 1, want: -2},
+		{a: -3, b: 4, want: 1},
+		{a: 3, b: -7, want: -4},
 	}
 	for _, testCase := range testCases {
 		got := calculator.Add(testCase.a, testCase.b)
