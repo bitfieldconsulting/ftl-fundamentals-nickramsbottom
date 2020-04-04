76bdabab77d5c46a42f78188034f87637652362d
Nick Ramsbottom <hi@nickramsbottom.com>
2020-04-04 16:40:47 +0100

    Implement calculator functionality

---
+++ 09/calculator.go
@@ -0,0 +1,16 @@
+package calculator
+
+// Subtract two numbers
+func Subtract(a, b int) int {
+	return a - b
+}
+
+// Multiply two numbers
+func Multiply(a, b int) int {
+	return a * b
+}
+
+// Divide two numbers
+func Divide(a, b int) int {
+	return a / b
+}
